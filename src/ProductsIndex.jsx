61443db6023920a1e5cf2811262ab3bcc4c87590
Peter Jang <peter.x.jang@gmail.com>
2023-03-01 18:30:01 -0600

    Style index as cards

--- src/ProductsIndex.jsx
+++ src/ProductsIndex.jsx
@@ -2,14 +2,23 @@ export function ProductsIndex(props) {
   return (
     <div id="products-index">
       <h1>All products</h1>
-      {props.products.map((product) => (
-        <div key={product.id}>
-          <h1>{product.name}</h1>
-          <img src={product.images[0].url} alt="" />
-          <p>{product.description}</p>
-          <p>{product.price}</p>
-        </div>
-      ))}
+      <div className="row">
+        {props.products.map((product) => (
+          <div key={product.id} className="col-sm-4">
+            <div className="card">
+              <img src={product.images[0].url} className="card-img-top" alt="..." />
+              <div className="card-body">
+                <h5 className="card-title">{product.name}</h5>
+                <p className="card-text">Price: {product.price}</p>
+                <p className="card-text">{product.description}</p>
+                <a href="#" className="btn btn-primary">
+                  Go somewhere
+                </a>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
